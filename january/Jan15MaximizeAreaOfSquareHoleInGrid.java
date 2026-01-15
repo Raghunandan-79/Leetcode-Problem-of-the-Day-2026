5981d6f6c315eef9b7113de9b9fdb6b624c2cd70
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-15 07:47:13 +0530

    January 15 problem added

---
+++ january/Jan15MaximizeAreaOfSquareHoleInGrid.java
@@ -0,0 +1,59 @@
+package january;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Jan15MaximizeAreaOfSquareHoleInGrid {
+    class Solution {
+        public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+            // Sort both horizontal and vertical bars
+            Arrays.sort(hBars);
+            Arrays.sort(vBars);
+
+            // Find the longest consecutive sequence in horizontal bars
+            int maxH = 1, maxV = 1, curr = 1;
+
+            for (int i = 1; i < hBars.length; i++) {
+                // If current bar is consecutive to previous, increment counter
+                if (hBars[i] == hBars[i - 1] + 1) curr++;
+                else curr = 1;
+                maxH = Math.max(maxH, curr);
+            }
+
+            // Find the longest consecutive sequence in vertical bars
+            curr = 1;
+
+            for (int i = 1; i < vBars.length; i++) {
+                // If current bar is consecutive to previous, increment counter
+                if (vBars[i] == vBars[i - 1] + 1) curr++;
+                else curr = 1;
+                maxV = Math.max(maxV, curr);
+            }
+
+            // The maximum square side is limited by the smaller dimension
+            int side = Math.min(maxH + 1, maxV + 1);
+
+            // Return the area of the square
+            return side * side;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), m = scanner.nextInt();
+        int[] hBars = new int[n], vBars = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            hBars[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            vBars[i] = scanner.nextInt();
+        }
+
+        Solution solution = new Jan15MaximizeAreaOfSquareHoleInGrid().new Solution();
+        System.out.println(solution.maximizeSquareHoleArea(n, m, hBars, vBars));
+
+        scanner.close();
+    }
+}
