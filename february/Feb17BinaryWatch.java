2b21285eaeda736b9b301caf0f09518d5a8c74b8
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-18 00:14:09 +0530

    February 17 problem added

---
+++ february/Feb17BinaryWatch.java
@@ -0,0 +1,44 @@
+package february;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Feb17BinaryWatch {
+    class Solution {
+        public List<String> readBinaryWatch(int turnedOn) {
+            List<String> result = new ArrayList<>();
+
+            for (int hour = 0; hour < 12; hour++) {
+
+                for (int minute = 0; minute < 60; minute++) {
+
+                    int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+
+                    if (totalBits == turnedOn) {
+
+                        String time = hour + ":" +
+                                (minute < 10 ? "0" + minute : minute);
+
+                        result.add(time);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int turnedOn = scanner.nextInt();
+
+        Solution solution = new Feb17BinaryWatch().new Solution();
+        for (var num : solution.readBinaryWatch(turnedOn)) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        scanner.close();
+    }
+}
